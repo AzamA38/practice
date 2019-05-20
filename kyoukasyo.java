fc31f19a0b3c32704b9d079c1897b0cc328a4d10
e185731 <e185731@ie.u-ryukyu.ac.jp>
2019-05-21 08:41:35 +0900

    first commit

---
+++ kyoukasyo.java
@@ -0,0 +1,12 @@
+public class kyoukasyo {
+	public static void main(String[] args) {
+		int n,m,k;
+		double x = 52.76;
+		n = (int) 123456789L;
+		m = (int)x;
+		k = (int)(x + 3.8);
+		System.out.println(n);
+		System.out.println(m);
+		System.out.println(k);
+	}
+}
